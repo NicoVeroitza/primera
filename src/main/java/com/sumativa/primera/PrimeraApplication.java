1d5aa25898eec51a46aa5aee899eadb51aedbc10
Raths <Raths@PCHIQUITA>
2024-08-26 06:04:50 -0400

    Primer commit

---
+++ src/main/java/com/sumativa/primera/PrimeraApplication.java
@@ -0,0 +1,13 @@
+package com.sumativa.primera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeraApplication.class, args);
+	}
+
+}
